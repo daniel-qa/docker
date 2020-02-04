8016e368cb32437d62649f3ba67e4931fb503820
daniel <daniel.lin.gr@gmail.com>
2020-02-04 11:35:54 +0800

    Update mysql.sh

--- base_dockerfile/mysql.sh
+++ base_dockerfile/mysql.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 docker build --build-arg db_root_pw=rootpw
 
+
+# 指定 檔案
+# docker build -f dockerfile_mysql
+
+
+# 指定 tag
+# docker build -t myqpp:v1  .
+
+
 # Must tag first
 docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql

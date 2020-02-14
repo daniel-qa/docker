dd388d19262092264f5f38c18b1a4da389a1b66a
daniel <daniel.lin.gr@gmail.com>
2020-02-14 14:40:24 +0800

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # ubuntu
-docker run -it -p --name myos -p 5800:80 5022:22 -v /opt/htdocs/ies:/opt/lampp/htdocs/ ubuntu:16.04 /bin/bash
+docker run -it --name myubuntu -p 5800:80 5022:22 -v /opt/htdocs/ies:/opt/lampp/htdocs/ ubuntu:16.04 /bin/bash
+
+
+# cnetos
+docker run -it -p --name myos -p 127.0.0.1::80 127.0.0.1:22 -v /opt/htdocs/ies:/opt/lampp/htdocs/ centos:6.8 /bin/bash
 

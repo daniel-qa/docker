118312fbb9bf83b2cfdcac61bc6df2b84cabd36c
daniel <daniel.lin.gr@gmail.com>
2020-02-14 14:44:23 +0800

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -5,5 +5,5 @@ docker run -it --name myubuntu -p 5800:80 5022:22 -v /opt/htdocs/ies:/opt/lampp/
 
 
 # cnetos
-docker run -it --name myos -p 127.0.0.1::80 127.0.0.1:22 -v /opt/htdocs/ies:/opt/lampp/htdocs/ centos:6.8 /bin/bash
+docker run -it --name myos -p 127.0.0.1::80 -p 127.0.0.1:22 -v /opt/htdocs/ies:/opt/lampp/htdocs/ centos:6.8 /bin/bash
 

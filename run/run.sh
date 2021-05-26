9ba5a836661c67509506d275fdbf05df6c02788c
daniel <daniel.lin.gr@gmail.com>
2021-05-26 09:16:29 +0800

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -7,3 +7,8 @@ docker run -it --name myubuntu -p 5800:80 5022:22 -v /opt/htdocs/ies:/opt/lampp/
 # cnetos
 docker run -it --name myos -p 127.0.0.1::80 -p 127.0.0.1:22 -v /opt/htdocs/ies:/opt/lampp/htdocs/ centos:6.8 /bin/bash
 
+
+# mysql 5.7
+docker run --name sokdb -p 3306:3306 -v /opt/dockerfile/mysql57/volume1:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 --restart=always -d mysql:5.7
+
+

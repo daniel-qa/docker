2160dc56abbf2455d7f30e4a26002a162c45afe3
daniel <daniel.lin.gr@gmail.com>
2021-05-28 17:29:16 +0800

    Create run_db.sh

---
+++ Docker Compose/mysql_admin/run_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# To start the application stack, in the same folder as the docker-compose.yml file, run:
+
+# Start the application stack defined in docker-compose.yml
+docker-compose up -d
+
+# Similar to docker run, the -d flag lets us run our application stack in the background. The command to stop and remove all containers and networks is:
+
+# Stop the application stack
+docker-compose down
